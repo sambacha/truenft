2026bb4318e96c496d32e0ab66865140d54bf1e8
sam bacha <sam@freighttrust.com>
2020-08-17 23:06:03 -0700

    solc compile (#1)

--- ethereum/contracts/nft/NFTRecordsExtension.sol
+++ ethereum/contracts/nft/NFTRecordsExtension.sol
@@ -1,12 +1,4 @@
-/* SPDX-License-Identifier: MPL-2.0
- * SPDXVersion: SPDX-2.2
- * SPDX-FileCopyrightText: Copyright 2020 (C) FreightTrust and Clearing Corporation
- * All Rights Reserved - https://freighttrust.com 
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/ */
-
-pragma solidity >=0.4.24 <0.7.0;
+pragma solidity 0.4.24;
 
 import "./NFTStandard.sol";
 
